c1025d859890d89e59ab8c4c75621c3bfa90f41e
alexmaunder <alex_maunder@hotmail.com>
2020-06-01 21:48:30 +1000

    added JS logic to check if naught or cross is first & update player board

--- js/project0.js
+++ js/project0.js
@@ -1,20 +1,49 @@
 console.log($)
 
+const checkForWin = function () {
+  // if 3 in a row, strike through & display "X Wins!" etc
+};
+
+
 const render = function () {
   // set game back to default but keep current score.
-  $(".start_game").addClass("animate__animated animate__bounce");
+  // $(".start_game").addClass("animate__animated animate__bounce");
+  // check if
 };
 
 $(document).ready(function () {
-  $('#a3').on('click', function () {
-    $('#a3').addClass('xmark').slideDown('slow');
+
+  // if any td's are clicked
+  $('td').on('click', function () {
+    if (this.className !== 'xmark' && this.className !== 'circle') {
+      let xOrO = game.naughtOrCross();
+      // game.test($('td').index(this)); // get index of td that's been clicked.
+      $(this).addClass(`${xOrO}`).slideDown('slow');
+    };
   });
-  $('#b3').on('click', function () {
-    $('#b3').addClass('xmark').slideDown('slow');
+
+  // determine player 1's character
+  $('#buttonX').on('click', function () {
+    game.whosFirst('buttonX');
   });
-  $('#c3').on('click', function () {
-    $('#c3').addClass('circle').slideDown('slow');
+
+  $('#buttonO').on('click', function () {
+    game.whosFirst('buttonO');
   });
+  //
+  // $('#a3').on('click', function () {
+  //   $('#a3').addClass('xmark').slideDown('slow');
+  // });
+  //
+  // $('#b3').on('click', function () {
+  //   $('#b3').addClass('xmark').slideDown('slow');
+  // });
+  //
+  // $('#c3').on('click', function () {
+  //   $('#c3').addClass('circle').slideDown('slow');
+  // });
+
+  // Restart Game
   $('.restart_game_button').on('click', function () {
     $("td").css('background', 'none');
     $("td").removeClass('xmark circle');
--- js/project_logic.js
+++ js/project_logic.js
@@ -1,9 +1,36 @@
-turnCount = 1 // start from turn 1
+let xMarksTurn = true;
+let turnCount = 0; // start from turn 1
 
-const turn = function () {
-  if (turnCount % 2 === 0) { // if player 2's turn "Crosses"
 
-  } else { // player 1's turn "Naughts"
+const game = {  // use an object to keep code neatly grouped...
 
+  // See if x or o is first player
+  whosFirst: function (input) {
+    if (input === 'buttonO') {
+      xMarksTurn = false
+    } else if (input === 'buttonX') {
+      xMarksTurn = true
+    } else { // means x is default first player
+      xMarksTurn = true
+    };
+    console.log('who first');
+    console.log(xMarksTurn);
+    return // return something here
+  },
+
+  // figure out if need to return x or o
+  naughtOrCross: function (index) {
+    if (xMarksTurn === true) { // if player 2's turn "Crosses"
+      console.log('xmark');
+      turnCount++;
+      xMarksTurn = false;
+      return 'xmark'
+    } else { // player 1's turn "Naughts"
+      console.log('circle');
+      turnCount++;
+      xMarksTurn = true;
+      return 'circle'
+    }
   }
-}
+
+};
