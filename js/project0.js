c7f668f46e7d2409b5ed9011b76a6b8c92f29de8
alexmaunder <alex_maunder@hotmail.com>
2020-06-01 17:23:13 +1000

    updated CSS and svg to display naughts and crosses

--- js/project0.js
+++ js/project0.js
@@ -1,17 +1,25 @@
 console.log($)
 
 const render = function () {
-  $('.restart_game_button').on('click', function () {
-    // set game back to default but keep current score.
-  });
+  // set game back to default but keep current score.
+  $(".start_game").addClass("animate__animated animate__bounce");
 };
 
 $(document).ready(function () {
   $('#a3').on('click', function () {
-    $('#a3').css('background-color', 'red')
+    $('#a3').addClass('xmark').slideDown('slow');
+  });
+  $('#b3').on('click', function () {
+    $('#b3').addClass('xmark').slideDown('slow');
   });
   $('#c3').on('click', function () {
-    $('#c3').css('background-color', 'green');
+    $('#c3').addClass('circle').slideDown('slow');
   });
-  $('.restart_game_button').on(render());
+  $('.restart_game_button').on('click', function () {
+    $("td").css('background', 'none');
+    $("td").removeClass('xmark circle');
+    $(".start_game").removeClass("animate__animated animate__bounce");
+    render();
+  });
+  // render();
 });
